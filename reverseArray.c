528d04472aaf2b19d9cb38b4f66e94f29645b00f
Adrianrising <112772850+Adrianrising@users.noreply.github.com>
2022-09-05 08:01:13 +0630

    Add files via upload
    Assignment 3

---
+++ reverseArray.c
@@ -0,0 +1,47 @@
+#include "stdio.h"
+/// program to reverse the array
+void swap(int* a,int* b);
+void reverse(int array[],int arraySize);
+void printArr(int* array,int arraySize);
+
+int main(){
+    int size;
+    int array[size];
+    printf("Enter the size of the array: ");
+    scanf("%d",&size);
+    for(int i=0;i<size;i++){
+        printf("Enter the number: ");
+        scanf("%d",&array[i]);
+    }
+
+
+    printf("original array-->");
+    printArr(array,size);
+    printf("\nREversed array-->");
+    reverse(array,size);
+    printArr(array,size);
+    return 0;
+}
+void swap(int* a,int* b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void reverse(int array[],int arraySize){
+    int* pointer1=array;
+    int* pointer2=array+arraySize-1;
+    while(pointer1<pointer2){
+        swap(pointer1,pointer2);
+        pointer1++;
+        pointer2--;
+    }
+}
+
+void printArr(int* array,int arraySize){
+    int* length=array+arraySize;
+    int* position=array;
+    for(position=array;position<length;position++){
+        printf("%d ",*position);
+    }
+}
